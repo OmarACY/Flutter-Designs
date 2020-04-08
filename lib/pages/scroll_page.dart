41dc00c9f7ccc7631d5205813d91cb862b87eaf8
Omar Alejandro Cervantes Yepez <omar@aumenta.mx>
2020-04-08 17:02:21 -0500

    Diseño con scroll - parte 3

--- lib/pages/scroll_page.dart
+++ lib/pages/scroll_page.dart
@@ -15,14 +15,53 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Center(
-      child: Text('Página 1'),
+    return Stack(
+      children: <Widget>[
+        _backgroundColor(),
+        _backgroundImage(),
+        _texts()
+      ],
     );
   }
 
   Widget _page2() {
     return Center(
       child: Text('Página 2'),
+    );
+  }
+
+  Widget _backgroundColor() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+          image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _texts() {
+
+    final textStyle = TextStyle( color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Text('11°', style: textStyle),
+          Text('Miércoles', style: textStyle),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white)
+        ],
+      ),
     );
   }
 }
