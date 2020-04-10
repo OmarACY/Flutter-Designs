90b68bad5b76fdfda5950a8494641703a5f9c10b
Omar Alejandro Cervantes Yepez <omar@aumenta.mx>
2020-04-10 10:13:20 -0500

    138. Diseño compuesto - Statusbar y titulos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutterdesigns/pages/basic_page.dart';
+import 'package:flutterdesigns/pages/buttons_page.dart';
 import 'package:flutterdesigns/pages/scroll_page.dart';
 
 void main() => runApp(MyApp());
@@ -8,13 +10,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+//    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
+//      statusBarColor: Colors.white,
+//    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'buttons',
       routes: {
         'basic' : (BuildContext context) => BasicPage(),
         'scroll': (BuildContext context) => ScrollPage(),
+        'buttons': (BuildContext context) => ButtonsPage(),
       },
     );
   }
